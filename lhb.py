3dee7d42b033010689ffe774c53e4ce295bb7848
RyanRen <yan.r@163.com>
2018-07-29 15:53:02 +0800

    add DB insertion

--- lhb.py
+++ lhb.py
@@ -17,29 +17,43 @@ import MySQLdb
 from datetime import date, timedelta
 
 
-REQUEST_TIMEOUT = 10
-REQUEST_RETRY = 3
-REQUEST_SLEEP = 0.001
+REQUEST_TIMEOUT = 2
+REQUEST_RETRY = 30
+REQUEST_SLEEP = 0.1
 
 REQUEST_ERROR_MSG = '重复请求已达最大次数，无法获取数据！'
 
 
-def LHB_Stock_Details(content, date):
+def LHB_Stock_Details(content, date, SCode, DBConnection):
+    if content == None:
+        return
+    insert_value = ''
     content = content.decode('GBK')
     # print(content)
     html = lxml.html.parse(StringIO(content))
     div_list = html.xpath(u"//div[@class=\"content-sepe\"]")
     # for item in div_list:
     # 	print(etree.tostring(item).decode('utf-8'))
+    if(len(div_list) < 1):
+        print("Not find LHB Table")
+        return
 
     for item_div in div_list:
         table_list = item_div.xpath(u"./table/tbody")
+        if (len(table_list) < 1):
+            print("Not find LHB table")
+            continue
+
         # print("table count %d"%(len(table_list)))
         for item_t in table_list:
             # print('-------print table content-------')
             # print(etree.tostring(item_t).decode('utf-8'))
             # item_t = lxml.html.parse(StringIO(etree.tostring(item_t).decode('utf-8')))
             row_list = item_t.xpath(u"./tr")
+            if (len(row_list) < 1):
+                print("Not find LHB YYB Item")
+                continue
+
             # print('row count %d'%(len(row_list)))
             for item_row in row_list:
                 colume_list = item_row.xpath('./td')
@@ -49,32 +63,63 @@ def LHB_Stock_Details(content, date):
 
                     # print(colume_list[0].text)
                     # 营业部名称
-                    print((colume_list[1].xpath(
-                        "./div[@class=\"sc-name\"]/a"))[1].text)
+                    YYB = (colume_list[1].xpath(
+                        "./div[@class=\"sc-name\"]/a"))[1].text
+                    # print((colume_list[1].xpath("./div[@class=\"sc-name\"]/a"))[1].text)
+
                     # 营业部代码
-                    # item = colume_list[1].xpath("./div/input")
-                    print(colume_list[1].xpath(
-                        "./div/input")[0].attrib['value'])
+                    YYBID = colume_list[1].xpath(
+                        "./div/input")[0].attrib['value']
+                    # print(colume_list[1].xpath("./div/input")[0].attrib['value'])
                     if colume_list[1].xpath("./div/input")[0].attrib['value'] == '':
-                        print("机构席位10000")
+                        YYBID = '10000000'
+                        # print("机构席位10000")
+
                     # 买入金额（万）
-                    print(colume_list[2].text)
-                    if(colume_list[2] == None):
-                        BMoney = 0
+                    BUY = colume_list[2].text
+                    # print(colume_list[2].text)
+                    if colume_list[2].text == None or colume_list[2].text == '-':
+                        BUY = '0'
 
                     # 买入占比
-                    print(colume_list[3].text)
+                    BRATE = colume_list[3].text[:-1]
+                    # print(colume_list[3].text)
+                    if colume_list[3].text == None or colume_list[3].text == '-':
+                    	BRATE = '0'
 
                     # 卖出金额（万）
-                    print(colume_list[4].text)
+                    SELL = colume_list[4].text
+                    # print(colume_list[4].text)
+                    if colume_list[4].text == None or colume_list[4].text == '-':
+                    	SELL = '0'
 
                     # 卖出占比
-                    print(colume_list[5].text)
+                    SRATE = colume_list[5].text[:-1]
+                    # print(colume_list[5].text)
+                    if colume_list[5].text == None or colume_list[5].text == '-':
+                    	SRATE = '0'
 
+                    insert_value = insert_value + '(\'%s\',\'%s\',%s,\'%s\',%s,%s,%s,%s),' % (date, SCode, YYBID, YYB, BUY, BRATE, SELL, SRATE)
+    try:
+        insert_value = insert_value[:-1]
+        #print insert_value
+        # Insert record
+        cursor = DBConnection.cursor()
+        insert = 'INSERT INTO LHB(LHBDATE, SCode, YYBID, YYB, BUY, BRATE, SELL, SRATE) VALUES' + insert_value
+        #print(insert)
+        cursor.execute(insert)
+    except Exception:
+    	print("%s, %s, %s, %s"%(colume_list[2].text, colume_list[3].text, colume_list[4].text, colume_list[5].text))
+    	print("%s, %s, %s, %s"%(BUY, BRATE, SELL, SRATE))
+    	print(insert)
+    	#DBConnection.roolback()
+    else:
+    	cursor.close()
+        DBConnection.commit()
     return
 
 
-def LHB_Stock_Info(content, date):
+def LHB_Stock_Info(content, date, DBConnection):
     if content == None:
         print('Content load is empty, quit!')
         return
@@ -101,7 +146,8 @@ def LHB_Stock_Info(content, date):
                         print("第%d次获取失败%s" % (_ + 1, e))
                     else:
                         print('成功获取股票代码%s:%s当天的龙虎榜数据！' % (SCode, date))
-                        LHB_Stock_Details(res.content, date)
+                        LHB_Stock_Details(res.content, date,
+                                          SCode, DBConnection)
                         break
                     if _ == 2:
                         print('经过多次尝试：无法获取股票代码%s:%s当天的龙虎榜数据！' % (SCode, date))
@@ -132,12 +178,23 @@ def LHB_Daily_Sumary(date):
 
 
 def DB_Setup():
+    # setup DB connection
     DBConnection = MySQLdb.connect(
         'localhost', 'ryan', 'renyan', 'LHB_DB', charset='utf8')
-    return
+
+    # Create Cursor
+    cursor = DBConnection.cursor()
+    # Create main LHB table
+    LHBColume = '(LHBDATE DATE NOT NULL, SCode VARCHAR(6) NOT NULL, 	YYBID INT NOT NULL, YYB VARCHAR(48) NOT NULL, BUY FLOAT, BRATE FLOAT, SELL FLOAT, SRATE FLOAT)'
+    CreateLHBTable = 'CREATE TABLE IF NOT EXISTS LHB' + LHBColume
+    cursor.execute(CreateLHBTable)
+    DBConnection.commit()
+
+    cursor.close
+    return DBConnection
 
 
-def LHB_Start():
+def LHB_Start(DBConnection):
     m_date = date.today()
     history = date(2000, 1, 1)
 
@@ -149,8 +206,9 @@ def LHB_Start():
 
         data = LHB_Daily_Sumary(m_date.__str__())
         # print(data)
-        LHB_Stock_Info(data, m_date.__str__())
+        LHB_Stock_Info(data, m_date.__str__(), DBConnection)
         m_date = m_date - timedelta(days=1)
+    return
 
 
 if __name__ == '__main__':
@@ -161,6 +219,7 @@ if __name__ == '__main__':
 
     print('龙虎榜数据分析程序V1.0')
 
-    LHB_Start()
+    hDB = DB_Setup()
+    LHB_Start(hDB)
 
     sys.exit(0)
